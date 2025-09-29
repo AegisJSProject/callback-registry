76b2f94ee97d7a82e4c050e3fb296a34836ae8e7
Chris Zuber <shgysk8zer0@gmail.com>
2025-09-29 08:28:16 -0700

    Add fullscreen, details, and command event support
    Introduces new UI functions for requesting, toggling, and exiting fullscreen, as well as opening and closing <details> elements. Adds support for the 'command' event and updates tests and examples to cover these new features. Also updates dependencies and documentation for v1.0.3.

--- callbackRegistry.test.js
+++ callbackRegistry.test.js
@@ -1,9 +1,10 @@
+import '@shgysk8zer0/polyfills';
 import './shims.js';
 import { createCallback, unregisterCallback, hasCallback, callCallback, closeRegistration } from '@aegisjsproject/callback-registry/callbacks.js';
 import { describe, test } from 'node:test';
 import assert from 'node:assert';
 
-const signal = AbortSignal.timeout(500);
+const signal = AbortSignal.timeout(1000);
 const sum = createCallback((...nums) => nums.reduce((sum, num) => sum + num));
 
 describe('Test callback registry', () => {
@@ -16,16 +17,16 @@ describe('Test callback registry', () => {
 	});
 
 	test('Callback can be called correctly.', { signal }, () => {
-		assert.equal(callCallback(sum, 1, 2 ,3), 6, 'Sum should be 6 when called with `1, 2, 3`.');
+		assert.strictEqual(callCallback(sum, 1, 2 ,3), 6, 'Sum should be 6 when called with `1, 2, 3`.');
 	});
 
 	test('Callbacks unregister', { signal }, () => {
 		unregisterCallback(sum);
-		assert.notEqual(hasCallback(sum), 'Callbacks should be removed from registry.');
+		assert.strictEqual(hasCallback(sum), false, 'Callbacks should be removed from registry.');
 	});
 
-	test('Cannot register new callbacks after closing registry', ({ signal }, () => {
+	test('Cannot register new callbacks after closing registry', { signal }, () => {
 		closeRegistration();
 		assert.throws(() => createCallback(() => 'test'), 'Creating callbacks should throw if registry is closed.');
-	}));
+	});
 });
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-import { onClick, onClose, observeEvents, onLoad, onError, registerSignal, signal as signalAttr, controller as controllerAttr, registerController } from '@aegisjsproject/callback-registry/events.js';
-import { createCallback, FUNCS, on } from '@aegisjsproject/callback-registry/callbacks.js';
+import { onClick, onClose, onCommand, observeEvents, onLoad, onError, registerSignal, signal as signalAttr, controller as controllerAttr, registerController, onDblclick } from '@aegisjsproject/callback-registry/events.js';
+import { createCallback, FUNCS, on, toggleFullsceen, requestFullscreen } from '@aegisjsproject/callback-registry/callbacks.js';
 
 const controller = new AbortController();
 const signal = registerSignal(controller.signal);
@@ -58,9 +58,15 @@ document.body.append(html`
 		<button type="button" ${onClick}="${FUNCS.navigate.reload}" ${signalAttr}="${signal}">Reload</button>
 		<button type="button" ${onClick}="${FUNCS.navigate.close}" ${signalAttr}="${signal}">Close</button>
 		<button type="button" ${onClick}="${FUNCS.ui.print}" ${signalAttr}="${signal}">Print</button>
+		<button type="button" ${onClick}="${FUNCS.ui.requestFullscreen}" ${signalAttr}="${signal}" ${requestFullscreen.attr}="container">Fullscreen</button>
+		<button type="button" ${onClick}="${FUNCS.ui.open}" ${signalAttr}="${signal}" data-open-selector="details">Open Details</button>
+		<button type="button" ${onClick}="${FUNCS.ui.close}" ${signalAttr}="${signal}" data-close-selector="details">Close Details</button>
 		<button type="button" ${onClick}="${FUNCS.ui.showModal}" data-show-modal-selector="#dialog" ${signalAttr}="${signal}">Show Dialog</button>
 		<button type="button" id="caf" ${onClick}="${FUNCS.ui.abortController}" ${controllerAttr}="${animController}" ${signalAttr}="${animSignal}">Cancel Animation</button>
 		<button type="button" ${onClick}="${() => alert('Hello, World!')}">Alert</button>
+		<button type="button" command="show-modal" commandfor="dialog">Show Dialog (command)</button>
+		<button type="button" command="show-popover" commandfor="popover">Show Popover</button>
+		<button type="button" command="--foo" commandfor="main">Dispatch <code>--foo</code></button>
 		<button type="button" ${on('click', event => {
 		const li = document.createElement('li');
 		li.textContent = `${event.type} @ ${event.timeStamp}`;
@@ -69,10 +75,12 @@ document.body.append(html`
 		<button type="button" ${on('click', () => controller.abort(), { signal })}>Abort</button>
 		<button type="button" ${on('click', event => event.target.parentElement.dispatchEvent(new CustomEvent('my:foo', { detail: event })), { signal })}>Foo</button>
 	</nav>
-	<main>
-		<div id="container">
+	<main id="main" ${onCommand}="${FUNCS.debug.log}">
+		<div id="container" ${onDblclick}="${FUNCS.ui.requestFullscreen}">
 			<template shadowrootmode="open">
 				<div part="container">
+					<button type="button" ${onClick}="${FUNCS.ui.toggleFullscreen}" ${toggleFullsceen.attr}="container">Toggle Fullscreen</button>
+					<br />
 					<slot name="list"></slot>
 					<pre part="events">[<slot name="events">No Events</slot>]</pre>
 					<img src="${URL.createObjectURL(getSvg())}"
@@ -85,11 +93,19 @@ document.body.append(html`
 			</template>
 			<ul id="list" slot="list"></ul>
 		</div>
+		<details id="details">
+			<summary>A &lt;details&gt;</summary>
+			<p> And here is the content.</p>
+		</details>
 	</main>
 	<footer></footer>
 	<dialog id="dialog" ${onClose}="${FUNCS.debug.log}" ${signalAttr}="${signal}">
 		<button type="button" ${onClick}="${FUNCS.ui.closeModal}" data-close-modal-selector="#dialog">Close</button>
+		<button type="button" command="request-close" commandfor="dialog">Close (command)</button>
 	</dialog>
+	<div popover="maual" id="popover">
+		<button type="button" command="hide-popover" commandfor="popover">Hide Popover</button>
+	</div>
 `);
 
 observeEvents(document.body);
